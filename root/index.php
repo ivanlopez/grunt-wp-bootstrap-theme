36ddfb01855617bb4c8f50e657bb8f2927fd7789
Ivan Lopez <ivan@10up.com>
2014-07-05 16:30:11 -0400

    add WordPress loop to index.php

--- root/index.php
+++ root/index.php
@@ -7,3 +7,11 @@
  */
  
  get_header(); ?>
+
+	<?php  if ( have_posts() ): ?>
+		<?php while ( have_posts() ): the_post();  ?>
+			
+		<?php endwhile; ?>
+	<?php endif; ?>
+
+ <?php get_footer(); ?>
